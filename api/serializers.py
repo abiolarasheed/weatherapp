b44ba758942005ab739e92d1239ba5658043145a
Abiola Rasheed <abiola@passthekeys.co.uk>
2019-03-11 12:20:15 +0000

    Updated WeatherForecastSerializer.data

--- api/serializers.py
+++ api/serializers.py
@@ -2,15 +2,19 @@ import json
 from typing import Dict, Any
 
 from rest_framework import serializers
-
 from api.models import WeatherForecast
 
 
 class WeatherForecastSerializer(serializers.ModelSerializer):
+    data = serializers.SerializerMethodField()
+
     class Meta:
         model = WeatherForecast
         fields = "__all__"
 
     @staticmethod
     def get_data(obj) -> Dict[Any, Any]:
+        """
+        Helper to convert sqlite text to dict
+        """
         return json.loads(obj.data)
